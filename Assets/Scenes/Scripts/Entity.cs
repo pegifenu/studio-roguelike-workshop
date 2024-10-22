662701638abd82322212ef4560e1b417bcc9e6ad
pegifenu <110061199+pegifenu@users.noreply.github.com>
2024-10-22 15:26:47 -0700

    projectile

--- Assets/Scenes/Scripts/Entity.cs
+++ Assets/Scenes/Scripts/Entity.cs
@@ -9,8 +9,40 @@ public class Entity : MonoBehaviour
     [Serializable]
     public struct EntityStats
     {
+        public float MaxHealth;
         public float MovementSpeed;
     }
 
+    [NonSerialized]
+    public float CurrentHealth;
+
+    bool dead = false;
+
     public EntityStats stats;
+
+    private void Start()
+    {
+        CurrentHealth = stats.MaxHealth;
+    }
+
+    private void Die()
+    {
+        dead = true;
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (dead)
+        {
+            return;
+        }
+
+        CurrentHealth -= Mathf.Ceil(damage);
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
 }
